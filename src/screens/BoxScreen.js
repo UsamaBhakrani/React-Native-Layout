eb0cc3874d6c5249ee6a462285b3e455fbda30f1
Mohammed Usama <usamabhakrani@gmail.com>
2024-03-09 15:11:43 +0500

    initial Commit

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+const BoxScreen = () => {
+  return (
+    <View>
+      <Text>BoxScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default BoxScreen;
